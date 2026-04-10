b4e2e81d3a9de7de7f5683918597ec2bd9c10454
Ritesh Baldva <riteshbaldva@Riteshs-MacBook-Pro.local>
2026-04-10 00:50:12 -0400

    Adding changes for my config on top of kickstarter

--- init.lua
+++ init.lua
@@ -110,10 +110,10 @@ vim.o.relativenumber = true
 vim.o.smartindent = true
 vim.o.wrap = false
 vim.o.colorcolumn = '80'
-vim.o.fillchars = {
+vim.opt.fillchars = {
   diff = '╱',
 }
-vim.o.diffopt = {
+vim.opt.diffopt = {
   'vertical',
   'internal',
   'filler',
@@ -301,14 +301,10 @@ require('lazy').setup({
     dependencies = {
       {
         'ldelossa/litee.nvim',
-        config = function()
-          require('litee.lib').setup()
-        end,
+        config = function() require('litee.lib').setup() end,
       },
     },
-    config = function()
-      require('litee.gh').setup()
-    end,
+    config = function() require('litee.gh').setup() end,
   },
 
   -- NOTE: Plugins can also be added by using a table,
@@ -480,9 +476,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
       vim.keymap.set('n', '<leader>se', builtin.git_status, { desc = '[S]earch [E]dited files' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-      vim.keymap.set('n', '<leader>sF', function()
-        builtin.lsp_document_symbols()
-      end, { desc = '[S]earch [F]unctions }' })
+      vim.keymap.set('n', '<leader>sF', function() builtin.lsp_document_symbols() end, { desc = '[S]earch [F]unctions }' })
 
       -- This runs on LSP attach per buffer (see main LSP attach function in 'neovim/nvim-lspconfig' config for more info,
       -- it is better explained there). This allows easily switching between pickers if you prefer using something else!
@@ -1029,7 +1023,7 @@ require('lazy').setup({
     config = function()
       -- ensure basic parser are installed
       local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go', 'python', 'rust' }
-      require('nvim-treesitter').install(parsers)
+      require('nvim-treesitter.install').ensure_installed(parsers)
 
       ---@param buf integer
       ---@param language string
@@ -1048,7 +1042,6 @@ require('lazy').setup({
         vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
       end
 
-      local available_parsers = require('nvim-treesitter').get_available()
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
           local buf, filetype = args.buf, args.match
@@ -1056,17 +1049,12 @@ require('lazy').setup({
           local language = vim.treesitter.language.get_lang(filetype)
           if not language then return end
 
-          local installed_parsers = require('nvim-treesitter').get_installed 'parsers'
-
-          if vim.tbl_contains(installed_parsers, language) then
-            -- enable the parser if it is installed
-            treesitter_try_attach(buf, language)
-          elseif vim.tbl_contains(available_parsers, language) then
-            -- if a parser is available in `nvim-treesitter` auto install it, and enable it after the installation is done
-            require('nvim-treesitter').install(language):await(function() treesitter_try_attach(buf, language) end)
-          else
-            -- try to enable treesitter features in case the parser exists but is not available from `nvim-treesitter`
-            treesitter_try_attach(buf, language)
+          -- try to attach treesitter; if parser isn't installed, auto-install it
+          local ok = pcall(vim.treesitter.start, buf, language)
+          if not ok then
+            local available = require('nvim-treesitter.parsers').available_parsers()
+            if vim.tbl_contains(available, language) then require('nvim-treesitter.install').ensure_installed { language } end
+            pcall(vim.treesitter.start, buf, language)
           end
         end,
       })
@@ -1135,9 +1123,7 @@ require('lazy').setup({
   },
   {
     'karb94/neoscroll.nvim',
-    config = function()
-      require('neoscroll').setup {}
-    end,
+    config = function() require('neoscroll').setup {} end,
   },
 }, {
   ui = {
