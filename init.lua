b23973d49ed74a872632b46954696c86a1b4fed6
Ritesh Baldva <baldvaritesh@gmail.com>
2026-04-16 23:45:53 -0400

    Enabling bunch of stuff

--- init.lua
+++ init.lua
@@ -341,21 +341,21 @@ require('lazy').setup({
   -- options to `gitsigns.nvim`.
   --
   -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    ---@module 'gitsigns'
-    ---@type Gitsigns.Config
-    ---@diagnostic disable-next-line: missing-fields
-    opts = {
-      signs = {
-        add = { text = '+' }, ---@diagnostic disable-line: missing-fields
-        change = { text = '~' }, ---@diagnostic disable-line: missing-fields
-        delete = { text = '_' }, ---@diagnostic disable-line: missing-fields
-        topdelete = { text = '‾' }, ---@diagnostic disable-line: missing-fields
-        changedelete = { text = '~' }, ---@diagnostic disable-line: missing-fields
-      },
-    },
-  },
+  -- { -- Adds git related signs to the gutter, as well as utilities for managing changes
+  --   'lewis6991/gitsigns.nvim',
+  --   ---@module 'gitsigns'
+  --   ---@type Gitsigns.Config
+  --   ---@diagnostic disable-next-line: missing-fields
+  --   opts = {
+  --     signs = {
+  --       add = { text = '+' }, ---@diagnostic disable-line: missing-fields
+  --       change = { text = '~' }, ---@diagnostic disable-line: missing-fields
+  --       delete = { text = '_' }, ---@diagnostic disable-line: missing-fields
+  --       topdelete = { text = '‾' }, ---@diagnostic disable-line: missing-fields
+  --       changedelete = { text = '~' }, ---@diagnostic disable-line: missing-fields
+  --     },
+  --   },
+  -- },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
@@ -945,50 +945,50 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-    end,
-  },
-  --{
-  --  'catppuccin/nvim',
+  --{ -- You can easily change to a different colorscheme.
+  --  -- Change the name of the colorscheme plugin below, and then
+  --  -- change the command in the config to whatever the name of that colorscheme is.
+  --  --
+  --  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --  'folke/tokyonight.nvim',
   --  priority = 1000, -- Make sure to load this before all the other start plugins.
-  --  opts = {
-  --    flavor = 'mocha',
-  --    no_italic = true,
-  --    term_colors = true,
-  --    styles = {
-  --      comments = {},
-  --    },
-  --    integrations = {
-  --      telescope = {
-  --        enabled = true,
+  --  config = function()
+  --    ---@diagnostic disable-next-line: missing-fields
+  --    require('tokyonight').setup {
+  --      styles = {
+  --        comments = { italic = false }, -- Disable italics in comments
   --      },
-  --    },
-  --  },
+  --    }
 
-  --  config = function(_, opts)
-  --    require('catppuccin').setup(opts)
-  --    -- Set colorscheme after options
-  --    vim.cmd.colorscheme 'catppuccin-mocha'
+  --    -- Load the colorscheme here.
+  --    -- Like many other themes, this one has different styles, and you could load
+  --    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --    vim.cmd.colorscheme 'tokyonight-night'
   --  end,
   --},
+  {
+    'catppuccin/nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = {
+      flavor = 'mocha',
+      no_italic = true,
+      term_colors = true,
+      styles = {
+        comments = {},
+      },
+      integrations = {
+        telescope = {
+          enabled = true,
+        },
+      },
+    },
+
+    config = function(_, opts)
+      require('catppuccin').setup(opts)
+      -- Set colorscheme after options
+      vim.cmd.colorscheme 'catppuccin-mocha'
+    end,
+  },
 
   -- Highlight todo, notes, etc in comments
   {
@@ -1114,11 +1114,11 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommended keymaps
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommended keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
